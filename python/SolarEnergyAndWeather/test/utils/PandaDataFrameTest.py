76a4b661a4fb5c2928ae9cca435f18fc014b9eec
spedapalli <samba.pedapalli@gmail.com>
2023-05-04 18:36:18 -0700

    Fixed DataFrame issues when working with API directly. Prev worked only with mock data. Also added regression, and other plots

---
+++ python/SolarEnergyAndWeather/test/utils/PandaDataFrameTest.py
@@ -0,0 +1,33 @@
+import datetime
+import glob
+import unittest
+import pandas as pd
+from src.solarroof import CSVReader as csvReader
+
+class PyApisTest(unittest.TestCase):
+    def tes_DataFrameColumnFormat(self):
+        # filePath = "../../data/tesla"
+        filePath = "data/"
+        # kwhDateFrame = csvReader.getKwhForDateFromCsv(filePath)
+        #
+        # timeCharIndex = (kwhDateFrame.iloc[1]['Date time']).find('T')
+        # kwhDateFrame['Date time'] = pd.to_datetime(kwhDateFrame['Date time']).dt.date
+        # print(kwhDateFrame)
+
+    def test_ReadCsvDataFrameColumnFormat(self):
+        # -- read individual file and merge
+        filePath = "../../data/tesla"
+        csv_files = glob.glob('{}/*.{}'.format(filePath, 'csv'))
+        print(csv_files)
+
+        dfAllCsvData = pd.DataFrame()
+
+        for file in csv_files:
+           csvData = pd.read_csv(file)
+           csvData['Date time'] = pd.to_datetime(csvData['Date time']).dt.date
+           dfAllCsvData = dfAllCsvData.append(csvData, ignore_index=True)
+
+        print(dfAllCsvData)
+
+
+
